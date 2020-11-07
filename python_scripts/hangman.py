63c085341cd8325d3a50c7af28a4031e0dff7cd2
Liz <eamaccready@gmail.com>
2020-11-07 15:00:22 -0800

    Added feature to catch duplicate guesses.

--- python_scripts/hangman.py
+++ python_scripts/hangman.py
@@ -1,7 +1,6 @@
 # Code from Cory Altoff's self-taught programmer.
 # Modified to handle duplicate letters and to print to console a bit neater.
-# TODO: add feature to store previous guesses and say you already guessed.
-##that letter and don't progress the game.
+# as well as reporting duplicate guesses and not allowing them.
 
 def hangman(word):
     wrong = 0
@@ -17,27 +16,31 @@ def hangman(word):
     word_letters = list(word)
     board = ["__"] * len(word)
     win = False
+    guessed = []
     print("Welcome to Hangman!")
     while wrong < len(stages)-1:
         print("\n")
         msg = "Guess a letter: "
         guess = input(msg)
-        if guess in word_letters:
-            #cind = word_letters.index(guess)
-            for (i, word) in enumerate(word_letters):
-                if word == guess:
-                    board[i]= guess
-                    word_letters[i] = '$'
+        if not guess in guessed:
+            if guess in word_letters:
+                for (i, word) in enumerate(word_letters):
+                    if word == guess:
+                        board[i]= guess
+                        word_letters[i] = '$'
+            else:
+                wrong +=1
+            guessed.append(guess)    
+            print((" ".join(board)))
+            e = wrong + 1
+            print("\n".join(stages[0:e]))
+            if "__" not in board:
+                print("\nYou win!")
+                print(" ".join(board))
+                win = True
+                break
         else:
-            wrong +=1
-        print((" ".join(board)))
-        e = wrong + 1
-        print("\n".join(stages[0:e]))
-        if "__" not in board:
-              print("\nYou win!")
-              print(" ".join(board))
-              win = True
-              break
+            print("You already guessed {}. Pick another letter.".format(guess))
     if not win:
         print("\n".join(stages[0:wrong +1]))
         print("\nYou lose! The word was {}".format(word)) 
